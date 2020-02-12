9d129f4ea91dd5ff82f6b4215a96c9692ed9f310
dmitry.c <dmitry.c@jadexconsulting.com>
2020-02-12 16:04:47 +0200

    Login page

--- app/src/main/java/com/example/instagram/HomeActivity.kt
+++ app/src/main/java/com/example/instagram/HomeActivity.kt
@@ -1,15 +1,36 @@
 package com.example.instagram
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
+
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+        /*auth.signInWithEmailAndPassword("dimon_krem@mail.ru", "password")
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d(TAG, "signIn: success")
+                } else {
+                    Log.e(TAG, "signIn: failure", it.exception)
+                }
+            }*/
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
